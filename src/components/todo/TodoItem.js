d4891480c0f09d737a32354c30c0144d187cadec
dev <devyar@gmail.com>
2020-08-29 16:15:20 +0300

    start

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { faSquare, faCheckSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-function TodoItem({item, toggleCompleted}) {
+function TodoItem({item, toggleCompleted, remove}) {
 
     const isCompleted = () => item.done === true;
 
@@ -11,12 +11,29 @@ function TodoItem({item, toggleCompleted}) {
         toggleCompleted(item.id);
     }
 
+    const onDelete = () => {
+        remove(item.id);
+    }
+
+    const getDescStyle = () => {
+        return {
+            textDecoration: isCompleted() ? 'line-through' : 'none'
+        }
+    }
+
     return (
-        <div className={'list-group-item d-flex'}>
-            <div className={'mr-2'} onClick={onToggleCompleted}>
-                <FontAwesomeIcon icon={isCompleted() ? faCheckSquare : faSquare} />
+        <div className={'list-group-item d-flex align-items-center'}>
+            <div className={'mr-2'}>
+                <button className={'btn btn-link'} onClick={onToggleCompleted}>
+                    <FontAwesomeIcon icon={isCompleted() ? faCheckSquare : faSquare} />
+                </button>
             </div>
-            <div>{item.desc}</div>
+            <div className={'w-100'} style={getDescStyle()}>{item.desc}</div>
+            <div>
+                <button className={'btn btn-link'} onClick={onDelete}>
+                    <FontAwesomeIcon icon={faTrash}/>
+                </button>
+            </div>
         </div>
     );
 }
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -6,6 +6,10 @@ function TodoList() {
 
     console.log('todolist...');
 
+    const [form, setForm] = useState({
+        desc: ''
+    })
+
     const [items, setItems] = useState({
         list: [1, 2, 3],
         entities: {
@@ -14,6 +18,40 @@ function TodoList() {
             3: {id:3, desc: 'todo #3', done: false},
         }
     })
+
+    // Form:
+
+    const onChangeDesc = e => {
+        setForm({
+            desc: e.target.value
+        })
+    }
+
+    const canSubmitForm = () => form.desc.length > 2;
+
+    const onSubmitForm = e => {
+        e.preventDefault();
+
+        if(canSubmitForm()) {
+            const nextId = items.list.length + 1;
+            setItems({
+                ...items,
+                list: [...items.list, nextId],
+                entities: {
+                    ...items.entities,
+                    [nextId]: {
+                        id: nextId, desc: form.desc, done: false
+                    }
+                }
+            })
+            setForm({
+                desc: ''
+            })
+        }
+    }
+
+    // List...
+
 
     const toggleCompleted = (id) => {
         console.log('toggleCompleted', id)
@@ -29,19 +67,44 @@ function TodoList() {
         })
     }
 
-    const itemsList = () => {
-        console.log('itemsList')
+    const remove = id => {
+        console.log('remove', id)
+        const newEntities = {...items.entities}
+        delete newEntities[id];
+        setItems({
+            ...items,
+            list: items.list.filter(_id => _id !== id),
+            entities: newEntities
+        })
+    }
+
+    const todos = () => {
+        console.log('todos')
         return items.list.map(id => items.entities[id]);
     }
 
+    const listItems = todos().map(todo =>
+        <TodoItem key={todo.id} item={todo} toggleCompleted={toggleCompleted} remove={remove}/>)
+
     return (
         <div>
-            <ul className={'list-group'}>
-                {
-                    itemsList().map(todo =>
-                        <TodoItem key={todo.id} item={todo} toggleCompleted={toggleCompleted}/>
-                    )
-                }
+            <div className={'card'}>
+                <div className={'card-header'}>
+                    Add todo
+                </div>
+                <div className={'card-body'}>
+                    <form onSubmit={onSubmitForm}>
+                        <div className="input-group">
+                            <input value={form.desc} onChange={onChangeDesc} type="text" className="form-control" placeholder="What to do?"/>
+                            <div className="input-group-append">
+                                <button disabled={!canSubmitForm()} className="btn btn-primary" type="submit">add</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <ul className={'list-group mt-2'}>
+                {listItems}
             </ul>
         </div>
     );
