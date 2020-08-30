4ba6fb69ae36519e76ce2380eb4ba8e21e0a4da1
dev <devyar@gmail.com>
2020-08-30 18:45:28 +0300

    redux & api

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -2,20 +2,26 @@ import React from "react";
 
 import TodoItem from "./TodoItem";
 
-export default function TodoList ({ items, onToggle, onRemove, isLoading }) {
+export default function TodoList ({ items, onToggle, onRemove, isLoading, errorText }) {
     return (
         <div className="todo-list">
+            {
+                isLoading && <div className="text-center p-3 text-muted">Loading...</div>
+            }
+            {
+                !isLoading && !items.length && <div className="text-center p-3 text-info">There are no todos yet</div>
+            }
+            {
+                !isLoading && errorText && <div className="text-center p-3 text-error">{errorText}</div>
+            }
             <ul className="list-group">
-                {   items.length
-                    ? items.map(({id, text, completed, isToggling, isRemoving}) =>
+                { items.map(({id, text, completed, isToggling, isRemoving}) =>
                         <TodoItem key={id} description={text} isCompleted={completed}
                                   className="list-group-item"
                                   onToggleCompleted={onToggle(id, completed)}
                                   isToggling={isToggling}
                                   isRemoving={isRemoving}
-                                  onRemove={onRemove(id)}/>)
-                    : <div className="text-center p-3 text-muted">{isLoading ? 'Loading...' : 'There are no todos yet'}</div>
-                }
+                                  onRemove={onRemove(id)}/>) }
             </ul>
         </div>
     );
